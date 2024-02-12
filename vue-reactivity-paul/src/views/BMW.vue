dd46ca431f00d16f03e4e1b5c178dce3741e0dcd
paulmkrtichian <paulmkr22@gmail.com>
2024-02-12 09:23:04 -0500

    2/12

---
+++ vue-reactivity-paul/src/views/BMW.vue
@@ -0,0 +1,9 @@
+<script>
+const BMWmodels = [
+    {
+        model:"X1 SUV",
+        description: "The compact, yet intrepid, four-door SUV.",
+        startingprice: "$40,500",
+    }
+]
+</script>
